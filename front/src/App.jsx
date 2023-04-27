97b1504a1bd5151f29b26e1f087e84e5f8c63b32
DavePlayer <davepalyer2001@gmail.com>
2023-04-27 14:47:52 +0200

    initial commit

---
+++ front/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+// when you fetch data use line adres bellow instead of full address
+// import.meta.env.API_URL
+// later on changing api url from 127.0.0.1 will be done by changing just one line instead of 1000 lines
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      works
+    </>
+  )
+}
+
+export default App
