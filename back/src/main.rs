3ff1da74dfb615c8c2c761da557107345790e82d
DavePlayer <daveplayer2001@gmail.com>
2023-04-30 16:29:34 +0200

    initial server config

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer};
+
+#[get("/")]
+async fn index() -> String {
+    format!("sup bro!") // <- response with app_name
+}
+
+#[actix_web::main()]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
