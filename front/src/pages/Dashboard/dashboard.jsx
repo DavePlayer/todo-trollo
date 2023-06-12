a4a331992bfba8c472df21b919615a74731836b4
Jazgalor <jazgalor@wp.pl>
2023-06-12 18:28:41 +0200

    mapping groups (all - not yet filtered)

--- front/src/pages/Dashboard/dashboard.jsx
+++ front/src/pages/Dashboard/dashboard.jsx
@@ -1,11 +1,20 @@
+import { useSelector } from "react-redux";
 import { Navbar } from "../../shared/Navbar/navbar";
 
 export const Dashboard = () => {
+    const groups = useSelector((state) => state.groups);
     return(
         <div className="dashboard">
             <Navbar/>
             <div className="mainspace">
-                <p>In Progress</p>
+                {groups.loading && <p>Loading...</p>}
+                {!groups.loading && groups.data.map(group => (
+                    <div className="group" key={group.id}>
+                        <h2> {group.title} </h2>
+                        <p>TASKS</p>
+                        <p>Created by: {group.owner}</p>
+                    </div>
+                ))}
             </div>
         </div>
     );
