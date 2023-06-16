494191c4bc5ece753e5d95d5965034f1fdd6dbd0
Jazgalor <jazgalor@wp.pl>
2023-06-16 18:16:22 +0200

    asynch login promise

--- front/src/pages/Login/login.jsx
+++ front/src/pages/Login/login.jsx
@@ -12,7 +12,7 @@ export const Login = () => {
         e.preventDefault();
         console.log(form)
         if (form.login.length <= 0 || form.password.length <= 0)
-            return console.error("Something is not yes");
+            return console.error("Something is no yes");
         dispatch(fetchLogin(form));
     };
     const handleChange = (e) => {
