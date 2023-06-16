1ab0d3c714691f32f731521d28a8a12e1bc62281
Jazgalor <jazgalor@wp.pl>
2023-06-16 18:12:06 +0200

    asynch login promise

--- front/src/pages/Login/login.jsx
+++ front/src/pages/Login/login.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { fetchLogin } from "../../redux/reducers/user";
 
 export const Login = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.user);
     const [form, setForm] = useState({ login: "", password: "" });
     const handleClick = (e) => {
         e.preventDefault();
-        navigate("/dashboard");
+        console.log(form)
+        if (form.login.length <= 0 || form.password.length <= 0)
+            return console.error("some data is not filled");
+        dispatch(fetchLogin(form));
     };
     const handleChange = (e) => {
         let { name, value } = e.target;
@@ -17,21 +24,29 @@ export const Login = () => {
             };
         });
     };
+    useEffect(() => {
+        if (user.jwt.length > 0) navigate("/dashboard");
+    }, []);
     return (
-        <div className="formPage" >
-            <h1>User Login</h1>
-            <form>
-                <label htmlFor="login">
-                    <p>Email:</p>
-                    <input type="text" name="login" id="login" value={form.login} onChange={(e) => handleChange(e)} />
-                </label>
-                <label htmlFor="password">
-                    <p>Password:</p>
-                    <input type="password" name="password" id="password" value={form.password} onChange={(e) => handleChange(e)} />
-                </label>
-                <button onClick={(e) => handleClick(e)}>Less' go</button>
-            </form>
-            <Link to={'/register'}>Sign up</Link>
-        </div>
+        <>
+            {user.isLoggedIn ? (<Navigate to="/dashboard" />) : 
+            (
+                <div className="formPage" >
+                    <h1>User Login</h1>
+                    <form>
+                        <label htmlFor="login">
+                            <p>Email:</p>
+                            <input type="text" name="login" id="login" value={form.login} onChange={(e) => handleChange(e)} />
+                        </label>
+                        <label htmlFor="password">
+                            <p>Password:</p>
+                            <input type="password" name="password" id="password" value={form.password} onChange={(e) => handleChange(e)} />
+                        </label>
+                        <button onClick={(e) => handleClick(e)}>Less' go</button>
+                    </form>
+                    <Link to={'/register'}>Sign up</Link>
+                </div>
+            )}
+        </>
     );
 };
--- front/src/redux/reducers/user.jsx
+++ front/src/redux/reducers/user.jsx
@@ -1,11 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+    loading: true,
     id: "",
-    email: "basic@example.com",
+    email: "",
     isLoggedIn: false,
-    jwt: "x"
-}
+    jwt: ""
+};
+
+export const fetchLogin = createAsyncThunk(
+    '/login',
+    ({ login, password }) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const test = Math.floor(Math.random() * 10);
+                if (test < 5) {
+                    resolve({
+                        id: "0",
+                        email: "basic@example.com",
+                        jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwicGFzc3dvcmQiOiJwYXNzd29yZCIsImxvZ2luIjoiYmFzaWNAZXhhbXBsZS5jb20ifQ.fjQKIH0zywmQYEpyPtNpQyRo9h4AImLZVs_obHCkzkQ"
+                    });
+                } else {
+                    reject('Failed to fetch login')
+                }
+            },1000)
+        })
+    }
+);
 
 export const userSlice = createSlice ({
     name: "user",
@@ -13,15 +34,32 @@ export const userSlice = createSlice ({
     reducers: {
         logout: (state) => {
             console.log('logging out...');
+            localStorage.removeItem("token");
             return(state = {
                 id: "",
-                email: "basic@example.com",
+                email: "",
                 isLoggedIn: false,
                 jwt: ""
             });
         }
     },
-})
+    extraReducers: (builder) => {
+        builder.addCase(fetchLogin.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(fetchLogin.fulfilled, (state, action) => {
+            state.loading = false;
+            state.isLoggedIn = true;
+            state.id = action.payload.id;
+            state.email = action.payload.email;
+            state.jwt = action.payload.jwt;
+            localStorage.setItem("token", state.jwt);
+        });
+        builder.addCase(fetchLogin.rejected, (state, action) => {
+            console.error(action.error.message);
+        });
+    },
+});
 
 export const { logout } = userSlice.actions;
 
